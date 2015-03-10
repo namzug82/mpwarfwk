38b8fcfbd247056b50c128f2bb4d47a3b18a3035
rjsanchez <rjsanchezestevez@gmail.com>
2015-03-10 13:09:44 +0100

    second commit

---
+++ rjsanchez23/mpwarfwk/src/Bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo
+ * Date: 09/03/2015
+ * Time: 20:48
+ */
+
+class Bootstrap {
+
+
+    public function __construct(Request $request)
+    {
+        var_dump($request);
+    }
+}
