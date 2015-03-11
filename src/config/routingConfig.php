91205d906ad1023e9111f7b85dd8745a68f3fc9b
rjsanchez <rjsanchezestevez@gmail.com>
2015-03-11 21:07:40 +0100

    no message

--- src/config/routingConfig.php
+++ src/config/routingConfig.php
@@ -1,12 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ricardo
- * Date: 11/03/2015
- * Time: 20:17
- */
+
 return array (
     'Hello'   => array(
         'HelloController' => 'init'
+    ),
+    'adios' => array(
+        'HelloController' => 'close'
     )
 );
