c0da5bbfe24a738a3537f980d6d4ea1fea2c3f19
rjsanchez <rjsanchezestevez@gmail.com>
2015-03-12 19:23:22 +0100

    no message

---
+++ src/component/Config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo
+ * Date: 12/03/2015
+ * Time: 19:17
+ */
+
+namespace src\Component;
+
+
+class Config {
+    public static function detect(){
+        $config = require("../app/Config/appConfig.php");
+        var_dump($config["type"]);
+    }
+}
--- src/component/Routing.php
+++ src/component/Routing.php
@@ -8,6 +8,7 @@ class Routing {
     private $url;
     function __construct($url)
     {
+        var_dump(Config::detect());
         $this->routingConfig = require("../app/Config/routingConfig.php");
         $this->url = $url;
 
