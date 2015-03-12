a935a2e478610306e2cb1bc66cd9283d3db0e1c8
rjsanchez <rjsanchezestevez@gmail.com>
2015-03-12 13:12:18 +0100

    no message

--- src/component/Bootstrap.php
+++ src/component/Bootstrap.php
@@ -14,8 +14,9 @@ class Bootstrap {
         $route = new Routing($this->url());
         $controller = key($route->route());
         $newController = new $controller();
-        //var_dump($route->route()[$controller]);die();
-        $newController->$route->route()[$controller]();
+        $method = $route->route()[$controller];
+        var_dump($method);
+        //$newController->$route->route()[$controller]();
 
     }
     public function url(){
