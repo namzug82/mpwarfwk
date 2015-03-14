ac1b05d8f2fe142d9f6d61e41fb7ed4ab9edb0b6
rjsanchez <rjsanchezestevez@gmail.com>
2015-03-14 16:17:17 +0100

    routing

---
+++ src/component/Routes/RoutingPhp.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ricardo
+ * Date: 14/03/2015
+ * Time: 15:10
+ */
+
+namespace src\component\Routes;
+
+
+
+class RoutingPhp extends Routing{
+
+    protected function parseConfigToArray(){
+
+        $this->routingConfig = require("../app/Config/php/routingConfig.php");
+    }
+
+}
