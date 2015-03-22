241c8dcf1f6cc7d785e7496140df63a4521e9f83
Ricardo Sánchez <rjsanchezestevez@gmail.com>
2015-03-22 12:16:32 +0100

    varios

--- src/Component/Routes/Route.php
+++ src/Component/Routes/Route.php
@@ -23,25 +23,16 @@ class Route {
     }
 
 
-    /**
-     * @return mixed
-     */
     public function getParams()
     {
         return $this->params;
     }
 
-    /**
-     * @return mixed
-     */
     public function getController()
     {
         return $this->controller;
     }
 
-    /**
-     * @return mixed
-     */
     public function getMethod()
     {
         return $this->method;
